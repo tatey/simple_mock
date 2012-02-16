8c34bcaaee993d7498275913f66b3294f43a04d4
Tate Johnson <tate@tatey.com>
2012-02-16 23:26:48 +1000

    Only test to interface.

--- test/unit/mock_delegator_test.rb
+++ test/unit/mock_delegator_test.rb
@@ -27,37 +27,36 @@ class MockDelegatorTest < MiniTest::Unit::TestCase
     assert_equal [1, 2], delegator.push(2)
   end
 
-  def test_verify_returns_true_when_methods_are_called_as_expected
-    array = Array.new
-    delegator = MockDelegator.new array
-    delegator.expect :push, [1, 2], [Fixnum]
-    delegator.push 2
-    assert delegator.verify
-  end
-
-  def test_verify_raises_when_methods_are_not_called_as_expected
-    array = Array.new
-    delegator = MockDelegator.new array
-    delegator.expect :push, [1, 2], [Fixnum]
-    assert_raises(MockExpectationError) { delegator.verify }
-  end
-
-  def test_raises_when_methods_are_called_with_wrong_number_of_arguments
+  def test_expect_calls_register_on_tracer
+    tracer = MiniTest::Mock.new
+    tracer.expect :register, true, [Symbol, Array]
     delegator = MockDelegator.new Object.new
-    delegator.expect :test, true, [Object, Object]
-    assert_raises(MockExpectationError) { delegator.test Object.new }
+    delegator.__tracer = tracer
+    delegator.expect :plus_one, 2
+    assert tracer.verify
   end
 
-  def test_raises_when_methods_are_called_with_wrong_argument_types
+  def test_verify_is_forwarded_to_tracer
+    tracer = Class.new { define_method(:verify) { true } }.new
     delegator = MockDelegator.new Object.new
-    delegator.expect :test, true, [Fixnum]
-    assert_raises(MockExpectationError) { delegator.test String.new }
+    delegator.__tracer = tracer
+    assert_equal true, delegator.verify
   end
 
-  def test_missing_method_is_forwarded_to_delegate
+  def test_method_is_forwarded_to_delegate
     array = Array.new
     delegator = MockDelegator.new array
     assert delegator.respond_to?(:push)
     assert_equal [1], delegator.push(1)
+  end
+
+  def test_method_calls_assert_on_tracer
+    tracer = MockDelegator.new Tracer.new
+    tracer.expect :assert, true, [Symbol, Array]
+    delegator = MockDelegator.new Object.new
+    delegator.__tracer = tracer
+    delegator.expect :plus_one, 2
+    delegator.plus_one
+    assert tracer.verify
   end
 end
