10453220338e79ef992b093b01d49ca1625d8e37
Tate Johnson <tate@tatey.com>
2012-02-16 22:47:33 +1000

    SimpleMock.new returns SimpleMock::MockDelegator with argument or MiniTest::Mock without argument.

--- lib/simple_mock.rb
+++ lib/simple_mock.rb
@@ -4,3 +4,13 @@ require 'minitest/mock'
 require 'simple_mock/mock_delegator'
 require 'simple_mock/tracer'
 require 'simple_mock/version'
+
+module SimpleMock
+  def self.new object = nil
+    if object.nil?
+      MiniTest::Mock.new
+    else
+      MockDelegator.new object
+    end
+  end
+end
--- test/unit/simple_mock_test.rb
+++ test/unit/simple_mock_test.rb
@@ -2,7 +2,7 @@ require 'helper'
 
 class SimpleMockTest < MiniTest::Unit::TestCase
   def test_new_without_argument_returns_minitest_mock
-    assert_instance_of MiniTest::Mock, SimpleMock.new
+    assert_match 'MiniTest::Mock', SimpleMock.new.inspect
   end
 
   def test_new_with_argument_returns_mock_delegator
